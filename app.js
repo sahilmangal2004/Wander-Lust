fce01f60b9b1c9091774a88a82dc3d21987a3e7d
Sahil mangal <sahilmangal2004@@gmail.com>
2025-04-22 00:53:22 +0530

    Fixed MongoDB connection string issue and updated .env config

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate")
 const ExpressError = require("./utils/ExpressError.js")
-const session = require("express-session")
 const flash = require("connect-flash")
 const passport = require("passport")
 const LocalSrategy = require("passport-local")
 const User = require("./models/user.js")
 const expressLayouts = require('express-ejs-layouts');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+app
+
 
 const port = process.env.PORT || 8080
 
@@ -25,7 +30,7 @@ const userRouter = require("./routes/user.js")
 
 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlst";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
     .then(() => {
@@ -36,7 +41,7 @@ main()
     });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("views", path.join(__dirname, "views"));
@@ -50,8 +55,21 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate)
 app.use(express.static(path.join(__dirname, "public")))
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET
+    },
+    touchAfter: 24 * 3600,
+})
+
+store.on("error", (err) => {
+    console.log("ERROR in MONGO SESSION STORE", err)
+})
+
 const sessionOptions= {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -60,7 +78,6 @@ const sessionOptions= {
         httpOnly: true
     },
 }
-
 // app.get("/", (req, res) => {
 //     res.send("Hi, I am root");
 // });
